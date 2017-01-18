890a347c082d43a06056fca3c43d54d5adf55e86
b4lthazar <b4lthazar@users.noreply.github.com>
2017-01-18 11:27:02 -0800

    fix: make mouse movement faster

--- keyboards/ergodox/keymaps/b4lthazar/config.h
+++ keyboards/ergodox/keymaps/b4lthazar/config.h
@@ -8,6 +8,25 @@
 #undef TAPPING_TOGGLE
 #define TAPPING_TOGGLE 2
 
+/* faster mousekeys than the default */
+#undef MOUSEKEY_MOVE_DELTA
+#undef MOUSEKEY_WHEEL_DELTA
+#undef MOUSEKEY_DELAY
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_MAX_SPEED
+#undef MOUSEKEY_TIME_TO_MAX
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_MOVE_DELTA           5
+#define MOUSEKEY_WHEEL_DELTA          1
+#define MOUSEKEY_DELAY                0
+#define MOUSEKEY_INTERVAL             5
+#define MOUSEKEY_MAX_SPEED            7
+#define MOUSEKEY_TIME_TO_MAX         70
+#define MOUSEKEY_WHEEL_MAX_SPEED     16
+#define MOUSEKEY_WHEEL_TIME_TO_MAX   40
+
+
 #undef IGNORE_MOD_TAP_INTERRUPT
 
 #endif /* CONFIG_B4LTHAZAR_H */
