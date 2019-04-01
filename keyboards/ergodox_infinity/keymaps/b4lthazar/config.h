56209e2d0e79f596526b87ca40a414518f4a7d47
b4lthazar <b4lthazar@users.noreply.github.com>
2019-04-01 15:57:06 -0700

    fix: move ergodox and make it work

---
+++ keyboards/ergodox_infinity/keymaps/b4lthazar/config.h
@@ -0,0 +1,32 @@
+#ifndef CONFIG_B4LTHAZAR_H
+#define CONFIG_B4LTHAZAR_H
+#include "../../config.h"
+
+#undef  TAPPING_TERM
+#define TAPPING_TERM 200
+
+#undef TAPPING_TOGGLE
+#define TAPPING_TOGGLE 2
+
+/* faster mousekeys than the default */
+#undef MOUSEKEY_MOVE_DELTA
+#undef MOUSEKEY_WHEEL_DELTA
+#undef MOUSEKEY_DELAY
+#undef MOUSEKEY_INTERVAL
+#undef MOUSEKEY_MAX_SPEED
+#undef MOUSEKEY_TIME_TO_MAX
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_MOVE_DELTA          5
+#define MOUSEKEY_WHEEL_DELTA         1
+#define MOUSEKEY_DELAY               0
+#define MOUSEKEY_INTERVAL            10
+#define MOUSEKEY_MAX_SPEED           8
+#define MOUSEKEY_TIME_TO_MAX         50
+#define MOUSEKEY_WHEEL_MAX_SPEED     16
+#define MOUSEKEY_WHEEL_TIME_TO_MAX   40
+
+
+#undef IGNORE_MOD_TAP_INTERRUPT
+
+#endif /* CONFIG_B4LTHAZAR_H */
---
+++ keyboards/ergodox_infinity/keymaps/b4lthazar/readme.md
@@ -0,0 +1,13 @@
+To build and load:
+
+1. Plug only the left keyboard in and reset it.
+
+```
+make ergodox_infinity:b4lthazar:dfu-util
+```
+
+2. Plug only the right keyboard in and reset it.
+```
+make ergodox_infinity:b4lthazar:dfu-util MASTER=right
+```
+
