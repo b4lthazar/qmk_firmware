b7a455a0df4d78eeeb70e39f4cde5bce5b8fb852
b4lthazar <b4lthazar@users.noreply.github.com>
2019-04-06 09:26:58 -0700

    fix: disable sleep_led

--- keyboards/ergodox_infinity/rules.mk
+++ keyboards/ergodox_infinity/rules.mk
@@ -66,10 +66,10 @@ EXTRAKEY_ENABLE  = yes # Audio control and System control(+450)
 CONSOLE_ENABLE   = no  # Console for debug(+400)
 COMMAND_ENABLE   = yes # Commands for debug and configuration
 CUSTOM_MATRIX    = yes # Custom matrix file for the ErgoDox EZ
-SLEEP_LED_ENABLE = yes # Breathing sleep LED during USB suspend
+SLEEP_LED_ENABLE = no # Breathing sleep LED during USB suspend
 NKRO_ENABLE      = yes # USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 UNICODE_ENABLE   = yes # Unicode
-SWAP_HANDS_ENABLE= yes # Allow swapping hands of keyboard
+SWAP_HANDS_ENABLE= no # Allow swapping hands of keyboard
 
 CUSTOM_MATRIX = yes # Custom matrix file
 SERIAL_LINK_ENABLE = yes
