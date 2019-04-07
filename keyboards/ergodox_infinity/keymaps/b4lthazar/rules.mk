8208150b0f369fa9b5691b96a30b274109eb0b06
b4lthazar <b4lthazar@users.noreply.github.com>
2019-04-07 09:06:12 -0700

    wip: just disable the visualizer (and thus led backlights) for now

--- keyboards/ergodox_infinity/keymaps/b4lthazar/rules.mk
+++ keyboards/ergodox_infinity/keymaps/b4lthazar/rules.mk
@@ -11,12 +11,20 @@ NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: ht
 BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 MIDI_ENABLE = no            # MIDI controls
 AUDIO_ENABLE = no           # Audio output on port C6
-UNICODE_ENABLE = no         # Unicode
+UNICODE_ENABLE = yes        # Unicode
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 RGBLIGHT_ENABLE = no        # Enable WS2812 RGB underlight.
 
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
-# Combos not working yet
-COMBO_ENABLE = no
+
+SWAP_HANDS_ENABLE= no # Allow swapping hands of keyboard
+
+SERIAL_LINK_ENABLE = yes
+VISUALIZER_ENABLE = no
+LCD_ENABLE = yes
+LCD_BACKLIGHT_ENABLE = yes
+MIDI_ENABLE = no
+RGBLIGHT_ENABLE = no
+
